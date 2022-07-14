f4d989344c2c9917af9dcbd235b0ca991fde22ae
hkh9601 <hkh9601@gmail.com>
2022-07-14 21:25:09 +0900

    slack redirect url등록

--- routes/slack.js
+++ routes/slack.js
@@ -6,6 +6,6 @@ const {
 } = require("../controllers/slackController");
 
 router.route("/").post(resposeToSlack);
-router.route("/oauth").post(resposeToSlackOauth);
+router.route("/oauth").get(resposeToSlackOauth);
 
 module.exports = router;
